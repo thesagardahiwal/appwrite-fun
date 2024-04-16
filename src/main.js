db99732459992be33414d34ce9010ae0a561a4dc
Sagar Dahiwal <133154948+thesagardahiwal@users.noreply.github.com>
2024-04-16 16:55:49 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -92,59 +92,6 @@ io.on("connection", (socket) => {
   });
 });
 
-export default async ({ req, res }) => {
-  const { method, url, headers, body } = req;
-
-  // Convert the Appwrite Function request to a format that Express can handle
-  const request = new express.request({
-    method,
-    url,
-    headers,
-    body,
-  });
-
-  const response = new express.response();
-
-  // When the response is finished in Express, send it back via Appwrite Function's res
-  response.on("finish", () => {
-    res.status(response.statusCode).send(response._getData());
-  });
-
-  // Use Express to handle the request
-  app.handle(request, response);
-};
-
-// This is your Appwrite function
-// It's executed each time we get a request
-// export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
-
-  // You can log messages to the console
-  // log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  // error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  // if (req.method === 'GET') {
-  //   app.get("/", (req, res) => {
-  //     res.send("Welcome to Node")
-  //   })
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    // return res.send('Hello, World!');
-  // }
-
-  // `res.json()` is a handy helper for sending JSON
-  // return res.json({
-  //   motto: 'Build like a team of hundreds_',
-  //   learn: 'https://appwrite.io/docs',
-  //   connect: 'https://appwrite.io/discord',
-  //   getInspired: 'https://builtwith.appwrite.io',
-  // });
-// };
+server.listen(PORT, () => {
+  conslog.log("Server is listning...");
+})
